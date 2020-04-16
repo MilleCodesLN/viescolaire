53ac762ec887b1d9f975d12cd1b6d94084845ba7
MilleCodesLN <mille.helene@outlook.fr>
2020-04-16 15:20:42 +0200

    installation des modules

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const morgan = require("morgan");
+
+const PORT = 3000;
+const app = express();
+
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(cors());
+app.use(bodyParser.json());
+
+app.listen(PORT, function () {
+  console.log("Server running on localhost:" + PORT);
+});
